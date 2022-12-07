b26eea1df0c8c8498be2cfbc85c51c9c79d53ba2
dima.kostishen@gmail.com <dima.kostishen@gmail.com>
2022-12-07 17:39:40 +0200

    Change index css

--- lab-9--desktop/src/main.jsx
+++ lab-9--desktop/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './index.scss'
 import MainScreen from './screens/MainScreen'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
